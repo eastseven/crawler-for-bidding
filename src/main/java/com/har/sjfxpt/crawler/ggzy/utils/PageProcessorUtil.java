889ae2baa0322fa8dd9167f827f4de3890f4aba0
eastseven <eastseven@foxmail.com>
2017-10-31 14:23:31 +0800

    修改PageProcessorUtil，去掉废弃的方法，去掉script tag

--- src/main/java/com/har/sjfxpt/crawler/ggzy/utils/PageProcessorUtil.java
+++ src/main/java/com/har/sjfxpt/crawler/ggzy/utils/PageProcessorUtil.java
@@ -18,46 +18,14 @@ public final class PageProcessorUtil {
 
     final static String REGEX_TAG_STYLE = "((<style>)|(<style type=.+))((\\s+)|(\\S+)|(\\r+)|(\\n+))(.+)((\\s+)|(\\S+)|(\\r+)|(\\n+))(<\\/style>)";
 
-    /**
-     * 抽取标签文本内容
-     * 替换<code>extractText(Elements elements)</code>
-     *
-     * @param root
-     * @return
-     */
-    @Deprecated
-    public static String extractText(Element root) {
-        if (root == null) return null;
-
-        String html = root.html();
-        String formatContent = StringUtils.removeAll(html, REGEX_TAG_STYLE);
-        formatContent = StringUtils.removeAll(formatContent, "<o:p>|</o:p>");
-        formatContent = StringUtils.removeAll(formatContent, "<\\w+[^>]*>|</\\w+>|<!-{2,}.*?-{2,}>|(&nbsp;)");
-        return formatContent;
-    }
-
     public static String extractTextByWhitelist(Element root) {
         if (root == null) return null;
         String html = root.html();
-        String textContent = Jsoup.clean(html, Whitelist.none());
+        Whitelist whitelist = Whitelist.none();
+        whitelist.removeTags("script");
+        String textContent = Jsoup.clean(html, whitelist);
         textContent = StringUtils.removeAll(textContent, "<!-{2,}.*?-{2,}>|(&nbsp;)|<o:p>|</o:p>");
         return textContent;
-    }
-
-    /**
-     * 格式化指定标签，将标签属性全部剔除
-     *
-     * @param root
-     * @return <tag>文本内容1</tag><tag>文本内容2</tag><tag>文本内容3</tag>
-     */
-    @Deprecated
-    public static String formatElements(Element root) {
-        if (root == null) return null;
-        String html = root.html();
-        final String regex = "(style=\".*?\")|(width=\".*?\")|(height=\".*?\")|<!-{2,}.*?-{2,}>|(&nbsp;)|<o:p>|</o:p>";
-        String formatContent = StringUtils.removeAll(html, REGEX_TAG_STYLE);
-        formatContent = StringUtils.removeAll(formatContent, regex);
-        return formatContent;
     }
 
     public static String formatElementsByWhitelist(Element root) {
@@ -65,6 +33,7 @@ public final class PageProcessorUtil {
         String html = root.html();
         Whitelist whitelist = Whitelist.relaxed();
         whitelist.removeTags("style");
+        whitelist.removeTags("script");
         whitelist.removeAttributes("table", "style", "width", "height");
         whitelist.removeAttributes("td", "style", "width", "height");
         String formatContent = Jsoup.clean(html, whitelist);
@@ -72,7 +41,7 @@ public final class PageProcessorUtil {
         return formatContent;
     }
 
-   final static Pattern pattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}");
+    final static Pattern pattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}");
 
     /**
      * @return
