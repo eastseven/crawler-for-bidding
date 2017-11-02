e342fbd9a927e28c789a5b5c51374b6a41131a98
eastseven <eastseven@foxmail.com>
2017-11-02 08:16:58 +0800

    重构ZGShiYouPipeline

--- src/main/java/com/har/sjfxpt/crawler/petrochina/ZGShiYouPipeline.java
+++ src/main/java/com/har/sjfxpt/crawler/petrochina/ZGShiYouPipeline.java
@@ -1,6 +1,7 @@
 package com.har.sjfxpt.crawler.petrochina;
 
 import com.har.sjfxpt.crawler.ggzy.model.DataItemDTO;
+import com.har.sjfxpt.crawler.ggzy.model.SourceCode;
 import com.har.sjfxpt.crawler.ggzy.service.DataItemService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +17,11 @@ import static com.har.sjfxpt.crawler.ggzy.utils.GongGongZiYuanConstant.KEY_DATA_
 
 /**
  * Created by Administrator on 2017/10/31.
+ * @author luofei
  */
 @Slf4j
 @Component
-public class ZGShiYouPipeline implements Pipeline{
+public class ZGShiYouPipeline implements Pipeline {
 
     @Autowired
     ZGShiYouDataItemRepository zgShiYouDataItemRepository;
@@ -29,12 +31,12 @@ public class ZGShiYouPipeline implements Pipeline{
 
     @Override
     public void process(ResultItems resultItems, Task task) {
-        List<ZGShiYouDataItem> dataItemList=resultItems.get(KEY_DATA_ITEMS);
-        if(org.springframework.util.CollectionUtils.isEmpty(dataItemList)){
-            log.warn("zhongguoshiyou save nothing,{}",task.getSite());
-        }else {
-           zgShiYouDataItemRepository.save(dataItemList);
-            log.info("zhongguoshiyou save {} to mongodb",dataItemList.size());
+        List<ZGShiYouDataItem> dataItemList = resultItems.get(KEY_DATA_ITEMS);
+        if (org.springframework.util.CollectionUtils.isEmpty(dataItemList)) {
+            log.warn("{} save nothing", SourceCode.ZSY);
+        } else {
+            zgShiYouDataItemRepository.save(dataItemList);
+            log.info("{} save {} to mongodb", SourceCode.ZSY, dataItemList.size());
 
             List<DataItemDTO> dtoList = dataItemList.stream().map(dataItem -> dataItem.dto()).collect(Collectors.toList());
             dataItemService.save2BidNewsOriginalTable(dtoList);
