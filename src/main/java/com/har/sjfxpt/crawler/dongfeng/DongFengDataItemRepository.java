116068cb3cbd8b23504ed50676603436f932ca86
136144763 <luo4545@126.com>
2018-01-04 19:27:00 +0800

    修改东风招投

--- src/main/java/com/har/sjfxpt/crawler/dongfeng/DongFengDataItemRepository.java
+++ src/main/java/com/har/sjfxpt/crawler/dongfeng/DongFengDataItemRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * Created by Administrator on 2017/12/21.
- */
+ */@Deprecated
 public interface DongFengDataItemRepository extends MongoRepository<DongFengDataItem, String> {
 }
--- src/test/java/com/har/sjfxpt/crawler/core/DongFengPageProcessorTests.java
+++ src/test/java/com/har/sjfxpt/crawler/core/DongFengPageProcessorTests.java
@@ -1,5 +1,8 @@
 package com.har.sjfxpt.crawler.core;
 
+import com.har.sjfxpt.crawler.core.annotation.SourceModel;
+import com.har.sjfxpt.crawler.core.pipeline.HBasePipeline;
+import com.har.sjfxpt.crawler.core.utils.SourceConfigAnnotationUtils;
 import com.har.sjfxpt.crawler.dongfeng.DongFengPageProcessor;
 import com.har.sjfxpt.crawler.dongfeng.DongFengPipeline;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +11,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Spider;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.har.sjfxpt.crawler.core.utils.GongGongZiYuanConstant.THREAD_NUM;
 
@@ -25,6 +32,23 @@ public class DongFengPageProcessorTests {
 
     @Autowired
     DongFengPipeline dongFengPipeline;
+
+    @Autowired
+    HBasePipeline hBasePipeline;
+
+
+    @Test
+    public void testDongFengAnnotation() {
+        List<SourceModel> list = SourceConfigAnnotationUtils.find(dongFengPageProcessor.getClass());
+        list.forEach(sourceModel -> log.debug(">>>{}", sourceModel.getUrl()));
+
+        List<Request> requestList = list.parallelStream().map(SourceModel::createRequest)
+                .collect(Collectors.toList());
+        Spider.create(dongFengPageProcessor)
+                .addRequest(requestList.toArray(new Request[requestList.size()]))
+                .addPipeline(hBasePipeline)
+                .run();
+    }
 
     @Test
     public void testDongFeng() {
