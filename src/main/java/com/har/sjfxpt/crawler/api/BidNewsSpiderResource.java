39fb5e82d3d9fdfc51a2d5e6281da74cda1999ff
eastseven <eastseven@foxmail.com>
2018-01-11 10:23:18 +0800

    增加开、关spider的接口

--- src/main/java/com/har/sjfxpt/crawler/api/BidNewsSpiderResource.java
+++ src/main/java/com/har/sjfxpt/crawler/api/BidNewsSpiderResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import us.codecraft.webmagic.Spider;
 
 import java.util.Map;
 
@@ -24,24 +25,62 @@ public class BidNewsSpiderResource {
     @Autowired
     SpiderNewLauncher spiderNewLauncher;
 
+    private Map<String, Object> getSpiderResponse(Spider spider) {
+        Map<String, Object> response = Maps.newConcurrentMap();
+        response.put("status", spider.getStatus());
+        response.put("pageCount", spider.getPageCount());
+        response.put("site", spider.getSite());
+        response.put("startTime", new DateTime(spider.getStartTime()).toString("yyyy-MM-dd HH:mm:ss"));
+        response.put("threadAlive", spider.getThreadAlive());
+        response.put("uuid", spider.getUUID());
+        return response;
+    }
+
     @GetMapping("/{sourceCode}")
     public Object get(@PathVariable String sourceCode) {
         log.debug(">>> api get spider info, {}", sourceCode);
+        if (!sourceCode.startsWith("spider_")) {
+            sourceCode = "spider_" + sourceCode;
+        }
 
-        BidNewsSpider spider = spiderNewLauncher.getSpider(sourceCode);
+        BidNewsSpider spider = spiderNewLauncher.getSpider(sourceCode.toLowerCase());
         if (spider != null) {
-            spider.getStatus();
-            spider.getPageCount();
-            Map<String, Object> response = Maps.newConcurrentMap();
-            response.put("status", spider.getStatus());
-            response.put("pageCount", spider.getPageCount());
-            response.put("site", spider.getSite());
-            response.put("startTime", new DateTime(spider.getStartTime()).toString("yyyy-MM-dd HH:mm:ss"));
-            response.put("threadAlive", spider.getThreadAlive());
-            response.put("uuid", spider.getUUID());
-            return response;
+            return getSpiderResponse(spider);
         }
 
         return null;
     }
+
+    @GetMapping("/start/{sourceCode}")
+    public Object start(@PathVariable String sourceCode) {
+        if (!sourceCode.startsWith("spider_")) {
+            sourceCode = "spider_" + sourceCode;
+        }
+        BidNewsSpider spider = spiderNewLauncher.getSpider(sourceCode.toLowerCase());
+        if (spider == null) return null;
+
+        if (!spider.getStatus().equals(Spider.Status.Running)) {
+            spider.start();
+        }
+
+        return getSpiderResponse(spider);
+    }
+
+    @GetMapping("/stop/{sourceCode}")
+    public Object stop(@PathVariable String sourceCode) {
+        if (!sourceCode.startsWith("spider_")) {
+            sourceCode = "spider_" + sourceCode;
+        }
+
+        BidNewsSpider spider = spiderNewLauncher.getSpider(sourceCode.toLowerCase());
+
+        if (spider == null) return null;
+
+        if (spider.getStatus().equals(Spider.Status.Running)) {
+            spider.stop();
+        }
+
+        return getSpiderResponse(spider);
+    }
+
 }
