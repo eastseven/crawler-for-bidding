8115f909f450e9e102ff81815e5e5ed324527dd6
eastseven <eastseven@foxmail.com>
2018-01-12 10:15:05 +0800

    修改scp2s99脚本

--- scp2s99.sh
+++ scp2s99.sh
@@ -4,4 +4,4 @@ source ~/.profile
 
 mvn clean package -U -Dmaven.test.skip=true -Paliyun
 
-scp target/*.jar root@192.168.3.99:/data/har-crawler-ggzy.jar
+scp target/*.jar root@192.168.3.99:/data/bidding/app.jar
