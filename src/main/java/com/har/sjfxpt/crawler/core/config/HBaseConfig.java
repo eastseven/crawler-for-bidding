e504a090ddde279a490c2a91fd46a1fd6730c7f2
136144763 <luo4545@126.com>
2018-01-18 11:57:45 +0800

    拆分pipeline

--- src/main/java/com/har/sjfxpt/crawler/core/config/HBaseConfig.java
+++ src/main/java/com/har/sjfxpt/crawler/core/config/HBaseConfig.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/har/sjfxpt/crawler/core/pipeline/MongoPipeline.java
+++ src/main/java/com/har/sjfxpt/crawler/core/pipeline/MongoPipeline.java
@@ -1,12 +1,12 @@
 package com.har.sjfxpt.crawler.core.pipeline;
 
+import com.har.sjfxpt.crawler.core.annotation.Source;
 import com.har.sjfxpt.crawler.core.model.BidNewsOriginal;
-import com.har.sjfxpt.crawler.core.service.HBaseService;
+import com.har.sjfxpt.crawler.core.repository.BidNewsOriginalRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
@@ -18,18 +18,14 @@ import java.util.stream.Collectors;
 import static com.har.sjfxpt.crawler.core.processor.BasePageProcessor.KEY_DATA_ITEMS;
 
 /**
- * @author dongqi
- *         通用存储
+ * Created by Administrator on 2018/1/18.
  */
 @Slf4j
 @Component
-public class HBasePipeline implements Pipeline {
+public class MongoPipeline implements Pipeline {
 
     @Autowired
-    HBaseService HBaseService;
-
-    @Value("${app.hbase.save:true}")
-    private boolean save;
+    BidNewsOriginalRepository repository;
 
     @Override
     public void process(ResultItems resultItems, Task task) {
@@ -43,9 +39,7 @@ public class HBasePipeline implements Pipeline {
                 .filter(bidNewsOriginal -> StringUtils.isNotBlank(bidNewsOriginal.getFormatContent()))
                 .collect(Collectors.toList());
 
-        if (save) {
-            HBaseService.saveBidNewsOriginals(list);
-        }
+        // 只是为了快速查看数据，实际数据还是以HBase中的为准
+        repository.save(list);
     }
-
 }
